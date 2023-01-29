c10e4f6f017121e5538f920cf72e71695efda2f3
Maryanne Wachter <maryanne.wachter@gmail.com>
2023-01-29 11:05:51 -0800

    initial non working attempt at serial port write

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+// use sha2::{Sha256, Digest};
+use serialport;
+
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+    // let mut hasher = Sha256::new();
+    // let data = b"Hello world!";
+    // hasher.update(data);
+    // // `update` can be called repeatedly and is generic over `AsRef<[u8]>`
+    // hasher.update("String data");
+    // // Note that calling `finalize()` consumes hasher
+    // let hash = hasher.finalize();
+    // println!("Binary hash: {:?}", hash);
+
+    let mut port = serialport::new("/dev/tty.usbmodem3101", 115_200).open().expect("Failed to open port");
+    let output = "r1,r18\n\n".as_bytes();
+    port.write(output).expect("Write failed!");
 }
