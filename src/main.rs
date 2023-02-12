2d48a65f6816ede295a4a7609cae9e89c4380dc8
Joe Ardent <code@ardent.nebcorp.com>
2023-02-12 14:13:23 -0800

    even more simpler

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use bloorm::{execute_port, yhash};
 use clap::Parser;
 use std::collections::hash_map::DefaultHasher;
 use std::hash::Hasher;
@@ -24,6 +23,16 @@ struct Args {
     command: String,
 }
 
+fn execute_port(port_address: &str, serial_string: &str) {
+    let output = serial_string.as_bytes();
+    loop {
+        if let Ok(mut port) = serialport::new(port_address, 115_200).open() {
+            port.write_all(output).expect("Write failed!");
+            break;
+        }
+    }
+}
+
 fn main() {
     let args = Args::parse();
     let hashstring = args.directory + " " + &args.command;
@@ -31,12 +40,11 @@ fn main() {
     let board_size = 32;
     let mut serial_string = String::with_capacity(128);
 
-    let mut hasher = DefaultHasher::default();
-    hasher.write(hashstring.as_bytes());
-    let hashsum = hasher.finish();
-
     for offset in 0..3 {
-        let value = yhash(hashsum + offset) % board_size;
+        let mut hasher = DefaultHasher::default();
+        hasher.write(hashstring.as_bytes());
+        let value = (hasher.finish() + offset) % board_size;
+
         serial_string.push_str(color.as_str());
         serial_string.push_str(&value.to_string());
         serial_string.push(',');
